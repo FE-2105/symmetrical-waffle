87b052462e8a35e6aa3f13851993d59bc96bc826
FE-2105 <belicovfrontend@gmail.com>
2021-08-17 21:20:37 +0300

    Add lesson 24

---
+++ 24.Sass, npm/src/js/numberCheck.js
@@ -0,0 +1,8 @@
+function numberCheck(num){
+    if(!isNaN(num)){
+        return true
+    }
+    return false
+}
+
+module.exports = numberCheck;
---
+++ 24.Sass, npm/src/js/script.js
@@ -0,0 +1,8 @@
+console.log(32)
+const $ = require("jquery");
+
+const numberCheck = require("./numberCheck");
+
+console.log($("body"))
+
+console.log(numberCheck(2))
